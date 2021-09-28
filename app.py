bdeb8c8327dc8cca13d18091bc6150c5d1f76d60
Thomas <t.deurloo@brabantsedelta.nl>
2021-09-28 13:06:10 +0200

    loc is list

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if st.session_state.login:
     df = returndf(imeilist=loc, datefrom=start, dateto=end)
     st.plotly_chart(
         px.line(
-            df, color="imei", title=f"Gemeten onttrokken hoeveelheden; {imeitoname().get(loc)}"
+            df, color="imei", title=f"Gemeten onttrokken hoeveelheden; {imeitoname().get(loc[0])}"
         ).update_layout(
             height=600,
             yaxis_title="gemeten ontrokken hoeveelheid (l/s)",
