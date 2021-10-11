5c82ffdc79bf2ace0b331830eedf5f9d7b56a369
Thomas <t.deurloo@brabantsedelta.nl>
2021-10-11 14:16:14 +0200

    update

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ if st.session_state.login:
     # controls
     imeis = getallimei()
     meta = returnmeta()
-    names = meta[meta.imeis.isin(imeis)]
     controls = st.sidebar.expander("Filters", expanded=True)
     loc = controls.multiselect("Locatie", options=imeis, default=[imeis[0]], format_func=lambda x: getname(x))
     start = controls.date_input(
